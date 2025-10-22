57d3b8bb9a57b02578564500b5e5b7d79e3aef93
Lightbridge-KS <ki11ip0.s.a.s@gmail.com>
2025-10-22 20:57:16 +0700

    Init Project

---
+++ MathTools.CLI/Program.cs
@@ -0,0 +1,5 @@
+﻿using MathTools.Core;
+
+var calc = new Calculator();
+Console.WriteLine($"5 + 3 = {calc.Add(5, 3)}");
+Console.WriteLine($"4 × 7 = {calc.Multiply(4, 7)}");
---
+++ README.md
@@ -0,0 +1,3 @@
+# MathTool .NET
+
+> An educational repo for structuring CLI (`console`) and Class Library (`classlib`) in a single .NET solution 
